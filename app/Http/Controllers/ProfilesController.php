adfd3ca5018e71e18b0753955b09171f713395b6
SylviaAchieng <achiengsylvia157@gmail.com>
2024-02-27 09:12:39 +0300

    first commit

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index( \App\Models\User $user)
+    {
+         
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(\App\Models\User $user){
+        return view('profiles.edit' , compact('user'));
+
+    }
+}
